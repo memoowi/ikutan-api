9da41684d1d46df4982bec68e3f35c2c37681626
memoowi <megatenlike@gmail.com>
2026-01-06 16:43:05 +0545

    feat: register role middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
@@ -13,8 +14,16 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, string $role): Response
     {
-        return $next($request);
+        // Check if user is logged in and has the required role
+        if (Auth::check() && Auth::user()->role === $role) {
+            return $next($request);
+        }
+
+        return response()->json([
+            'status' => 'Error',
+            'message' => 'Forbidden: You do not have the ' . $role . ' role.',
+        ], 403);
     }
 }
