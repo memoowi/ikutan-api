f6ba2fe3fd158fb104159e452df4b97641eca463
memoowi <megatenlike@gmail.com>
2026-01-06 16:30:06 +0545

    feat: use Ulid for PK event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    use HasUlids;
     //
 }
