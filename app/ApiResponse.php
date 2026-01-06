674ee3a6faa3495b0d923f134078601b46d36701
memoowi <megatenlike@gmail.com>
2026-01-06 15:35:06 +0545

    feat: trait for uniform api response

---
+++ app/ApiResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponse
+{
+    protected function successResponse($data, $message = null, $code = 200): JsonResponse
+    {
+        return response()->json([
+            'status' => 'Success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function errorResponse($message = null, $code = 400): JsonResponse
+    {
+        return response()->json([
+            'status' => 'Error',
+            'message' => $message,
+            'data' => null
+        ], $code);
+    }
+}
